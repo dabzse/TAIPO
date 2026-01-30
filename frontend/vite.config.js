aa0ea26519faaa3c326ceea4594edfb49df8b3c0
Mihaly Nyilas <dr.dabzse@gmail.com>
2026-01-30 16:20:38 +0100

    update: vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
-import tailwindcss from "@tailwindcss/vite";
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [tailwindcss(), vue()],
+    plugins: [vue()],
     server: {
         proxy: {
             '/api': {
